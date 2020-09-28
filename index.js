4a1d6ce0b2a3cff995e2dfe3193e209c77860647
Joshua Saunders <jk.saunders@hotmail.com>
2020-09-28 16:12:14 -0400

    Updated to use splitLink

--- index.js
+++ index.js
@@ -1,71 +1,84 @@
+import fetch from 'cross-fetch';
 import { argv } from 'yargs';
 import * as ws from 'ws';
-import { ApolloClient, gql } from 'apollo-boost';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { WebSocketLink } from 'apollo-link-ws';
+import {
+  ApolloClient,
+  ApolloLink,
+  gql,
+  HttpLink,
+} from '@apollo/client';
+import { InMemoryCache } from '@apollo/client/cache';
+import { WebSocketLink } from '@apollo/client/link/ws';
 import { SubscriptionClient } from 'subscriptions-transport-ws';
 
+if (!argv.wsEndpoint) {
+  console.error('missing `wsEndpoint` argument');
+}
+if (!argv.httpEndpoint) {
+  console.error('missing `httpEndpoint` argument');
+}
+if (!argv.query && !argv.mutation) {
+  console.log('no `query` or `mutation` argument specified')
+  process.exit(1);
+}
+
 const wsClient = new SubscriptionClient(
-  argv.endpoint,
-  { lazy: false, reconnect: true },
+  argv.wsEndpoint,
+  {
+    lazy: true,
+    reconnect: true,
+  },
   ws,
   []
 );
 
-wsClient.onConnected(() => {
-  console.log('Connected to subscription client!');
+const splitLink = ApolloLink.split(
+  (operation) => {
+    return false;
+  },
+  new WebSocketLink(wsClient),
+  new HttpLink({
+    fetch,
+    uri: argv.httpEndpoint,
+  })
+);
 
-  const link = new WebSocketLink(wsClient);
+const apolloClient = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: ApolloLink.from([splitLink]),
+});
 
-  const apolloClient = new ApolloClient({
-    cache: new InMemoryCache(),
-    link,
-  });
-
-  const runTest = async () => {
-    if (argv.query) {
-      console.log('Start query test');
-      try {
-        const result = await apolloClient.query({
-          query: gql`${argv.query}`,
-        });
-        console.log('Apollo query success', JSON.stringify(result));
-      } catch (error) {
-        console.log('Apollo query error', JSON.stringify(error));
-        process.exit(1);
-      }
+const runTest = async () => {
+  if (argv.query) {
+    console.log('Start query test');
+    try {
+      const result = await apolloClient.query({
+        query: gql`${argv.query}`,
+      });
+      console.log('Apollo query success', JSON.stringify(result));
+    } catch (error) {
+      throw error;
+      console.log('Apollo query error', JSON.stringify(error));
+      process.exit(1);
     }
-
-    if (argv.mutation) {
-      console.log('Start mutation test');
-      try {
-        const result = await apolloClient.mutate({
-          mutation: gql`${argv.mutation}`,
-        });
-        console.log('Apollo mutation success', JSON.stringify(result));
-      } catch (error) {
-        console.log('Apollo mutation error', JSON.stringify(error));
-        process.exit(1);
-      }
-    }
-  };
-
-  if (!argv.query && !argv.mutation) {
-    console.log('No query or mutation specified')
-    process.exit(1);
   }
 
+  if (argv.mutation) {
+    console.log('Start mutation test');
+    try {
+      const result = await apolloClient.mutate({
+        mutation: gql`${argv.mutation}`,
+      });
+      console.log('Apollo mutation success', JSON.stringify(result));
+    } catch (error) {
+      console.log('Apollo mutation error', JSON.stringify(error));
+      process.exit(1);
+    }
+  }
+};
+
+runTest().then(() => {
   runTest().then(() => {
-    runTest().then(() => {
-      process.exit();
-    });
+    process.exit();
   });
 });
-wsClient.onError(error => {
-  console.log('Websocket client error', error)
-  process.exit(1);
-});
-wsClient.onDisconnected(error => {
-  console.log('Websocket client disconnected', JSON.stringify(error))
-  process.exit(1);
-})
